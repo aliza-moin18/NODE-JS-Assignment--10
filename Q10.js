63230a7b7c92be3b0ae86cb1877ce753b9394dfd
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-26 23:33:58 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,11 @@
+// Q10.  Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+var a = 40;
+var b = 50;
+// Addition
+console.log("".concat(a, "+").concat(b, "=").concat(a + b));
+// Subtraction
+console.log("".concat(a, "-").concat(b, "=").concat(a - b));
+//Multiplication
+console.log("".concat(a, "*").concat(b, "=").concat(a * b));
+//Division
+console.log("".concat(a, "/").concat(b, "=").concat(a / b));
---
+++ Q10.ts
@@ -0,0 +1,16 @@
+// Q10.  Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+let a: number = 40;
+let b: number = 50;
+
+// Addition
+console.log(`${a}+${b}=${a + b}`);
+
+// Subtraction
+console.log(`${a}-${b}=${a - b}`);
+
+//Multiplication
+console.log(`${a}*${b}=${a * b}`);
+
+//Division
+console.log(`${a}/${b}=${a / b}`);
